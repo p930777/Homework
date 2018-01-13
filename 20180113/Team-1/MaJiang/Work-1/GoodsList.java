35b9cf6235c5bd47d5599f769155642673c3de2c
Majiang2018 <majiang@promote.cache-dns.local>
2018-01-13 16:27:18 +0800

    增加了GoodsList

---
+++ 20180113/Team-1/MaJiang/Work-1/GoodsList.java
@@ -0,0 +1,15 @@
+/*
+ *@author  马江
+ *@Time 2018-1-13 16:20
+ *类名 GoodsList
+ *说明 购物清单
+ */
+public class GoodsList {
+	public static void main(String[] args) {
+		// 打印购物清单
+		System.out.println("\t\t购物清单\t\t");
+		System.out.println("商品名称\t\t购物数量\t\t商品单价\t\t金额");
+		System.out.println("鳄鱼夹克\t\t1\t\t1900\t\t1900");
+		System.out.println("ipoadMP4\t\t2\t\t400\t\t800");
+	}
+}
