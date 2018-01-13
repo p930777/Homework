82f58e48b6dd8809d7b43e278728394725aa179f
lc <lc@lc-PC>
2018-01-13 20:01:37 +0800

    在控制台输出购物系统登录菜单和系统主菜单

---
+++ 20180113/Team-1/ZhangLinChun/Work-2/LoginMenu.java
@@ -0,0 +1,21 @@
+/*
+ *MainMenu.java
+ *2018-1-13 15:20
+ *在控制台输出购物系统登录菜单
+ */
+public class LoginMenu {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("\t\t欢迎使用我行我素购物管理系统");
+		System.out.print("\n");
+		System.out.println("\t\t\t1.登录系统");
+		System.out.println("");
+		System.out.println("\t\t\t2.退出");
+		System.out.println("");
+		System.out.println("********************************************************************");
+		System.out.println("");
+		System.out.print("轻选择输入数字：");
+	}
+
+}
