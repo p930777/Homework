788e61941ac04e27b4fbadc824d1265f4af216ab
lc <lc@lc-PC>
2018-01-13 19:36:17 +0800

    增加print写法

--- 20180113/Team-1/ZhangLinChun/Work-1/GoodsList.java
+++ 20180113/Team-1/ZhangLinChun/Work-1/GoodsList.java
@@ -7,7 +7,8 @@
 public class GoodsList {
 	public static void main(String[] args) 
 	{
-		System.out.println("\t\t购物清单");
+		System.out.print("\t\t购物清单");
+		System.out.print("\n");
 		System.out.println("商品名称\t购物数量\t商品单价\t金额");
 		System.out.println("鳄鱼夹克\t   1\t\t1900\t\t1900");
 		System.out.println("ipodMP4\t\t   2\t\t400\t\t800");
