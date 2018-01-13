aa8cbca86659d2bdf03c1eeac8d5203888423991
lc <lc@lc-PC>
2018-01-13 19:30:42 +0800

    修改格式

--- 20180113/Team-1/ZhangLinChun/Work-1/GoodsList.java
+++ 20180113/Team-1/ZhangLinChun/Work-1/GoodsList.java
@@ -5,7 +5,8 @@
  *购物清单
  */
 public class GoodsList {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("\t\t购物清单");
 		System.out.println("商品名称\t购物数量\t商品单价\t金额");
 		System.out.println("鳄鱼夹克\t   1\t\t1900\t\t1900");
