e86d509e4d9db196a0416a611f68a8960930b035
Charlie_T <Charlie_T@192.168.0.107>
2018-01-15 21:59:54 +0800

    这是我完成的作业，请检测！

---
+++ 20180113/Team-1/TangWenBin/Work-1/GoodsPrice.java
@@ -0,0 +1,10 @@
+public class GoodsPrice
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\t\t\t购物清单");
+		System.out.println("商品名称" + "\t购买数量" + "\t商品单价" + "\t金额");
+		System.out.print("鳄鱼夹克" + "\t1" + "\t\t1900" + "\t\t1900\n");
+		System.out.println("ipodMP4" + "\t\t2" + "\t\t400" + "\t\t800");
+	}
+}
---
+++ 20180113/Team-1/TangWenBin/Work-2/ManagementSystemTwo.java
@@ -0,0 +1,14 @@
+public class ManagementSystemTwo
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\n\t\t欢迎使用我行我素购物管理系统");
+		System.out.println("\n*************************************************************************");
+		System.out.println("\n\t\t\t1.客户信息管理");
+		System.out.println("\n\t\t\t2.购物结算");
+		System.out.println("\n\t\t\t3.真情回馈");
+		System.out.println("\n\t\t\t4.注销");
+		System.out.println("\n*************************************************************************");
+		System.out.println("\n请选择，输入数字：");
+	}
+}
