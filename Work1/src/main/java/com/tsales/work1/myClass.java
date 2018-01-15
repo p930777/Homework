64186dea7aad09dcea571188499a024a07e549f6
wei79741151 <wz439651854@163.com>
2018-01-15 18:18:07 +0800

    delete

--- Work1/src/main/java/com/tsales/work1/myClass.java
+++
@@ -1,9 +0,0 @@
-package com.tsales.work1;
-
-import com.sun.corba.se.impl.encoding.EncapsOutputStream;
-
-public class myClass {
-    public static void main(String[] args) {
-        System.out.println("Helloworld!");
-    }
-}
