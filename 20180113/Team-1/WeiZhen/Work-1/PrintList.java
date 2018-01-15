3b8b458cd12bbd4ce54b7ecd85defb2f5a2eb62a
wei79741151 <wz439651854@163.com>
2018-01-15 18:14:51 +0800

    作业

---
+++ 20180113/Team-1/WeiZhen/Work-1/PrintList.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author tdsales-10
+ *
+ */
+
+public class PrintList {
+
+	public static void main(String[] args) {
+		System.out.println("\t购物清单");
+		System.out.println("商品名称   "+ '\t' +"购买数量" + '\t' + "商品单价" +'\t'+ "金额" + '\n');
+		System.out.println("鳄鱼夹克   "+ '\t' +"1" + '\t' + "1900" +'\t'+ "1900" + '\n');
+		System.out.println("ipodMP4"+ '\t' +"2" + '\t' + "400" +'\t'+ 2*400 + '\n');
+	}
+
+}
