acb6b1d1dcd25e92343a6725a9f328926aa1b916
bpe-16 <bpe-16@bpe-16.dc.local>
2018-01-17 17:47:51 +0800

    第一题

---
+++ 20180113/Team-1/CaoWei/Work-1/GoodsList.java
@@ -0,0 +1,10 @@
+public class GoodsList
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("\t\t\t购物清单\n");
+		System.out.print("商品名称\t购买数量\t商品单价\t金额\n");
+		System.out.print("鳄鱼夹克\t   1\t\t1900\t\t1900\n");
+		System.out.print("ipodMP4\t\t   2\t\t400\t\t800\n");
+	}
+}
