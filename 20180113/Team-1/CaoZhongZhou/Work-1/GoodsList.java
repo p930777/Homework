000b42867bfa74f5e2e42cb7053c82fb74001bcc
caozhongzhou <caozhongzhou1@163.com>
2018-01-19 08:21:09 +0800

    zuoye1

---
+++ 20180113/Team-1/CaoZhongZhou/Work-1/GoodsList.java
@@ -0,0 +1,11 @@
+public class GoodsList 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\t"+"\t"+"\t"+"购物清单");	
+		System.out.println("商品名称"+"\t"+"购买数量"+"\t"+"商品单价"+"\t"+"金额");
+		System.out.println("鳄鱼夹克"+"\t"+"   1    "+"\t"+"1900    "+"\t"+"1900");
+		System.out.println("ipodMP4" +"\t"+"\t"+"   2    "+"\t"+"400     "+"\t"+"800 ");
+	}
+	
+}
