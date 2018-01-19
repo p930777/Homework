d2f05978cde8067f7899aed51c3a3d7da7271eac
Administrator <Administrator@PC201405080803>
2018-01-19 13:27:36 +0800

    作业

---
+++ 20180113/Team-2/Zhangyu/Work-2/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String [] args)
+	{
+		System.out.println("\t欢迎使用我行我素购物管理系统\n");
+		System.out.println("************************************************");
+		System.out.println("\t\t1.客户信息管理\n");
+		System.out.println("\t\t2.购物结算\n");
+		System.out.println("\t\t3.真情回馈\n");
+		System.out.println("\t\t4.注销\n");
+		System.out.println("************************************************");
+		System.out.println("请选择，输入数字：\n");
+	}
+}
