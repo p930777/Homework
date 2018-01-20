2cc2e4a3484c524ec97b93dab62503d9d7bdf1d9
Administrator <Administrator@192.168.0.107>
2018-01-20 17:52:53 +0800

    作业

---
+++ 20180113/Team-1/WangRui/Work-1/HELLO.java
@@ -0,0 +1,14 @@
+
+public class HELLO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t购物清单");
+		System.out.println("商品名称\t购买数量\t商品单价");
+		System.out.println("鳄鱼夹克\t  1\t1900\t1900");
+		System.out.println("ipodMP4\t\t  2\t400\t800");
+
+
+	}
+
+}
