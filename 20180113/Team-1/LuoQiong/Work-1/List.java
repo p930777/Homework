877ec511982abfdbd6fc5cbea113b1b3cb310261
wei79741151 <wz439651854@163.com>
2018-01-22 13:43:54 +0800

    作业

---
+++ 20180113/Team-1/LuoQiong/Work-1/List.java
@@ -0,0 +1,13 @@
+
+public class List {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.print("\t\t购物清单\n");
+		System.out.println("商品名称\t\t购买数量\t\t商品单价\t\t金额");
+		System.out.println("鳄鱼夹克\t\t1\t\t1900\t\t1900");
+		System.out.println("ipodMP4\t\t2\t\t400\t\t800");
+	}
+
+}
+
---
+++ 20180113/Team-1/LuoQiong/Work-2/List3.java
@@ -0,0 +1,17 @@
+
+public class List3 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.print("\t\t欢迎使用我行我素购物管理系统\n");
+		System.out.print("\n");
+		System.out.print("\t\t1. 登录系统\n");
+		System.out.print("\n");
+		System.out.print("\t\t2. 退出\n");
+		System.out.print("\n");
+		System.out.print("* * * * * * * * * * * * * * * * * * * * * * * * *\n");
+		System.out.print("\n");
+		System.out.print("请选择，输入数字：\n");	
+	}
+
+}
